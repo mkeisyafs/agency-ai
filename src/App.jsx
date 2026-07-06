8937eba66fe62a3371c9188e6fca593801c8db35
mkeisyafs <mobilezero24@gmail.com>
2026-07-06 09:20:26 +0700

    fix capitallize

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
 import OurWork from './components/OurWork'
-import Teams from './components/teams'
+import Teams from './components/Teams'
 import ContactUs from './components/ContactUs'
 export const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
